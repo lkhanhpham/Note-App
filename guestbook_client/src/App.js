aef9e961f1c1ee2ce5ab18cfba72f72f0f039081
lpham1 <103462008+lpham1@users.noreply.github.com>
2024-07-12 19:23:04 +0200

    my project

---
+++ guestbook_client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+import Home from "./Home";
+
+function App() {
+    return (
+      <>
+        <Header />
+        <Home />
+      </>
+    );
+  }
+  
+  export default App;
